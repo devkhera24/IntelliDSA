3ad0d858d6d06510327286e1870733a7782332b9
Dev Khera <kheradev4@gmail.com>
2026-04-20 20:24:13 +0530

    Monaco code editor added with keystroke heatmap tracking

--- src/components/Editor/CodeEditor.jsx
+++ src/components/Editor/CodeEditor.jsx
@@ -1,1 +1,32 @@
-// TODO
+import Editor from '@monaco-editor/react'
+
+export default function CodeEditor({ value, onChange, onCursorChange, language = 'javascript' }) {
+	function handleMount(editor) {
+		editor.onDidChangeCursorPosition((e) => {
+			if (onCursorChange) {
+				onCursorChange(e.position.lineNumber, e.position.column)
+			}
+		})
+	}
+
+	return (
+		<Editor
+			height="100%"
+			language={language}
+			value={value}
+			theme="vs-dark"
+			onChange={onChange}
+			onMount={handleMount}
+			options={{
+				fontSize: 14,
+				minimap: { enabled: false },
+				scrollBeyondLastLine: false,
+				wordWrap: 'on',
+				lineNumbers: 'on',
+				padding: { top: 12, bottom: 12 },
+				renderLineHighlight: 'line',
+				smoothScrolling: true,
+			}}
+		/>
+	)
+}
